8f2e8db1d412df5b1800cb1d02481c75cd1e3fda
blorbb <88137137+blorbb@users.noreply.github.com>
2024-11-01 21:47:11 +1100

    expose custom host functions

--- qpmu-api/src/lib.rs
+++ qpmu-api/src/lib.rs
@@ -7,4 +7,25 @@ pub mod bindings {
     });
 }
 
-pub use bindings::{export, host, Guest as Plugin, ListItem, PluginAction};
+pub mod host {
+    use crate::bindings::host;
+
+    pub use host::{Capture, Output, SpawnError};
+
+    pub fn spawn(
+        cmd: &str,
+        args: impl IntoIterator<Item: AsRef<str>>,
+        capture: Capture,
+    ) -> Result<Output, SpawnError> {
+        host::spawn(
+            cmd,
+            &args
+                .into_iter()
+                .map(|a| a.as_ref().to_owned())
+                .collect::<Vec<_>>(),
+            capture,
+        )
+    }
+}
+
+pub use bindings::{export, Guest as Plugin, ListItem, PluginAction};

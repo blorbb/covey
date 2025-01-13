b94609cf8840af946b118aa74fa765a47e22b367
blorbb <88137137+blorbb@users.noreply.github.com>
2025-01-13 21:03:24 +1100

    auto resize window to body size

--- qpmu-tauri/src-tauri/src/lib.rs
+++ qpmu-tauri/src-tauri/src/lib.rs
@@ -1,14 +1,22 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn set_window_size(app: tauri::AppHandle, width: f64, height: f64) {
+    eprintln!("setting window size to {width}x{height}");
+    if let Some(window) = app.get_webview_window("main") {
+        window
+            .set_size(tauri::Size::Logical(tauri::LogicalSize { width, height }))
+            .unwrap();
+    } else {
+        eprintln!("WARN: main window was not found");
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![set_window_size])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

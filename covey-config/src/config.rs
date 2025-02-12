cb3b1cfc11b252593c32162d344c1a345dbf3860
blorbb <88137137+blorbb@users.noreply.github.com>
2025-02-12 20:44:14 +1100

    fix app config defaults

--- covey-config/src/config.rs
+++ covey-config/src/config.rs
@@ -22,14 +22,18 @@ pub struct GlobalConfig {
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[cfg_attr(feature = "ts-rs", derive(ts_rs::TS))]
 #[serde(rename_all = "kebab-case")]
+// every field must have a default annotation
+// so that a partially filled config still works
 pub struct AppConfig {
     /// Hotkey to re-initialise the current plugin.
     ///
     /// Default is Ctrl+R.
+    #[serde(default = "default_reload_hotkey")]
     pub reload_hotkey: Hotkey,
     /// List of icon themes to use when rendering a named icon from a plugin.
     ///
     /// Icons will try to be loaded from top to bottom.
+    #[serde(default = "default_icon_themes")]
     pub icon_themes: Vec<IconTheme>,
 }
 
@@ -62,19 +66,27 @@ pub enum IconThemeKind {
 impl Default for AppConfig {
     fn default() -> Self {
         Self {
-            reload_hotkey: Hotkey {
-                key: KeyCode::R,
-                ctrl: true,
-                alt: false,
-                shift: false,
-                meta: false,
-            },
-            icon_themes: vec![IconTheme {
-                kind: IconThemeKind::System,
-                name: "hicolor".to_string(),
-            }],
+            reload_hotkey: default_reload_hotkey(),
+            icon_themes: default_icon_themes(),
         }
     }
+}
+
+fn default_reload_hotkey() -> Hotkey {
+    Hotkey {
+        key: KeyCode::R,
+        ctrl: true,
+        alt: false,
+        shift: false,
+        meta: false,
+    }
+}
+
+fn default_icon_themes() -> Vec<IconTheme> {
+    vec![IconTheme {
+        kind: IconThemeKind::System,
+        name: "hicolor".to_string(),
+    }]
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]

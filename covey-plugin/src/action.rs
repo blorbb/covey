5a56d15788931d080a3f2b72b1519a1888a5b93c
blorbb <88137137+blorbb@users.noreply.github.com>
2025-02-22 22:46:23 +1100

    helper constructors for action + docs

--- covey-plugin/src/action.rs
+++ covey-plugin/src/action.rs
@@ -1,5 +1,13 @@
 use crate::{Input, proto};
 
+/// An action for Covey to perform.
+///
+/// There are associated constructor functions which are easier to use
+/// than constructing from the enum variants.
+///
+/// Note that actions don't close Covey automatically. Add [`Action::Close`] to
+/// close the application. This should usually be first to feel more responsive
+/// and avoid waiting for other commands to run before closing.
 #[derive(Debug, Clone)]
 pub enum Action {
     Close,
@@ -27,12 +35,42 @@ impl Action {
     }
 }
 
+/// Helper constructors for action variants
+impl Action {
+    pub fn close() -> Self {
+        Self::Close
+    }
+
+    pub fn run_command<Arg: Into<String>>(
+        cmd: impl Into<String>,
+        args: impl IntoIterator<Item = Arg>,
+    ) -> Self {
+        Self::RunCommand(cmd.into(), args.into_iter().map(Into::into).collect())
+    }
+
+    pub fn run_shell(script: impl Into<String>) -> Self {
+        Self::RunShell(script.into())
+    }
+
+    pub fn copy(str: impl Into<String>) -> Self {
+        Self::Copy(str.into())
+    }
+
+    pub fn set_input(input: impl Into<Input>) -> Self {
+        Self::SetInput(input.into())
+    }
+}
+
 /// Wrapper for a [`Vec<Action>`] with convenient conversion trait implementations.
 ///
 /// [`From`] Implementations:
 /// - [`IntoIterator<Item = Action>`] -> `Vec<Action>`
 /// - [`Action`] -> `vec![Action]`
 /// - [`Input`] -> `vec![Action::SetInput(Input)]`
+///
+/// While each action will be run in sequence, they will not wait for previous
+/// actions to complete. If you need to run something more complex,
+/// you can write your desired code in the command callback.
 pub struct Actions {
     pub(crate) list: Vec<Action>,
 }

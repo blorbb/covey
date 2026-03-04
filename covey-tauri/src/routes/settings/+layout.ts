e9273036cca16f0873e86d1a8b4140a768298706
blorbb <88137137+blorbb@users.noreply.github.com>
2026-03-04 12:26:54 +1100

    delete covey tauri

--- covey-tauri/src/routes/settings/+layout.ts
+++
@@ -1,9 +0,0 @@
-import { Settings } from "$lib/settings.svelte";
-
-import type { LayoutLoad } from "./$types";
-
-// menu must be initialised in page load (not layout load),
-// to avoid being re-initialised when settings page is opened.
-export const load: LayoutLoad = async () => ({
-  settings: await Settings.new(),
-});
